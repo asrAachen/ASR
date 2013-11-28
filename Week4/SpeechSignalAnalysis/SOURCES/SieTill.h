bf2f57d42513927020bc7c89a9dd17748bbf12df
asrAachen <subramanyam86@gmail.com>
2013-11-28 10:23:21 +0100

    Added Week 4 base code

---
+++ Week4/SpeechSignalAnalysis/SOURCES/SieTill.h
@@ -0,0 +1,19 @@
+/*****************************************************************************/
+/*                                                                           */
+/* File: SieTill.h                                                           */
+/*                                                                           */
+/* COPYRIGHT (C) Lehrstuhl fuer Informatik VI                                */
+/*               RWTH Aachen                                                 */
+/*                                                                           */
+/* Authors: W. Macherey                                                      */
+/*                                                                           */
+/* Project: Lehrstuhl Praktikum                                              */
+/*                                                                           */
+/* Last Update: Sam Feb  8 00:30:49 CET 2003                                 */
+/*                                                                           */
+/*****************************************************************************/
+
+#ifndef __SIETILL_H__
+#define __SIETILL_H__
+
+#endif /* __SIETILL_H__ */
